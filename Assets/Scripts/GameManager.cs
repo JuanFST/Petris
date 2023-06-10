3a2882314577eafa82fb81ed979c7e0c468a97b4
Juan Marcos Bianchi Matsumoto <forestarg@gmail.com>
2023-06-10 19:29:04 -0300

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private Transform blockPrefab;
+    [SerializeField] private Transform blockHolder;
+
+    [SerializeField] private TMPro.TextMeshProUGUI livesText;
+
+    private Transform currentBlock = null;
+    private Rigidbody2D currentRigidbody;
+
+    private Vector2 blockStartPosition = new Vector2(0f, 4f);
+
+    [SerializeField] private float blockSpeed = 10f;
+    [SerializeField] private float blockSpeedIncrement = 0.5f;
+    [SerializeField] private int blockDirection = 1;
+    [SerializeField] private float xLimit = 5;
+
+
+    //Variables de estado
+
+    private int startingLives = 3;
+    private int livesRemaining;
+    private bool playing = true;
+
+
+    private void Start()
+    {
+        livesRemaining = startingLives;
+        livesText.text = $"{livesRemaining}";
+        SpawnNewBlock();
+    }
+
+    private void Update()
+    {
+        if (currentBlock && playing)
+        {
+            float moveAmount = Time.deltaTime * blockSpeed * blockDirection;
+            currentBlock.position = currentBlock.position + new Vector3(moveAmount, 0, 0);
+
+            if (Mathf.Abs(currentBlock.position.x) > xLimit)
+            {
+                currentBlock.position = new Vector3(blockDirection * xLimit, currentBlock.position.y, 0);
+                blockDirection = -blockDirection;
+            }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                currentBlock = null;
+                currentRigidbody.simulated = true;
+                SpawnNewBlock();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+        }
+
+    }
+    public void removeLife()
+    {
+        livesRemaining = Mathf.Max(livesRemaining - 1, 0);
+        livesText.text = $"{livesRemaining}";
+
+        if(livesRemaining == 0)
+        {
+            playing = false;
+        }
+    }
+
+
+    private void SpawnNewBlock()
+    {
+        currentBlock = Instantiate(blockPrefab, blockHolder);
+        currentBlock.position = blockStartPosition;
+        currentRigidbody = currentBlock.GetComponent<Rigidbody2D>();
+
+        blockSpeed = blockSpeed + blockSpeedIncrement;
+    }
+}
